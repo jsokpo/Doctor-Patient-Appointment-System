c77be69221c40022e681310014f537f66d18089b
John Sokpo <john.sokpo@hotmail.com>
2025-08-05 17:03:36 +0100

    Update ClinicAndSpecialities.jsx

--- src/components/Home/ClinicAndSpecialities/ClinicAndSpecialities.jsx
+++ src/components/Home/ClinicAndSpecialities/ClinicAndSpecialities.jsx
@@ -9,51 +9,34 @@ import './index.css';
 
 const ClinicAndSpecialities = () => {
 	return (
-		<section className="section section-specialities position-relative">
+		<section className="section-specialities position-relative">
 			<div className="container-fluid">
-				<div className='mb-5 section-title text-center'>
+				<div className="mb-5 section-title text-center">
 					<h2>Clinic and Specialities</h2>
-					<p className='m-0'>Our Clinic and Specialities showcase our desire to be more than just doctors. We are fully committed and enlightened on the evolving health strategies to living and well-being.</p>
+					<p className="m-0">
+						Our Clinic and Specialities showcase our desire to be more than just doctors. 
+						We are fully committed and enlightened on the evolving health strategies to living and well-being.
+					</p>
 				</div>
 
 				<div className="row justify-content-center">
 					<div className="col-md-9">
-						<div className="specialities-slider slider d-flex justify-content-center align-items-center gap-5">
-							<div className="speicality-item text-center">
-								<div className="speicality-img">
-									<img src={img1} className="img-fluid" alt="" />
-									<span><i><FaCheckDouble/></i></span>
+						<div className="specialities-slider d-flex flex-wrap justify-content-center align-items-center gap-4">
+							{[
+								{ img: img1, name: "Urology" },
+								{ img: img2, name: "Neurology" },
+								{ img: img3, name: "Orthopedic" },
+								{ img: img4, name: "Cardiologist" },
+								{ img: img5, name: "Dentist" }
+							].map((item, idx) => (
+								<div className="speciality-item" key={idx}>
+									<div className="speciality-img">
+										<img src={item.img} alt={item.name} />
+										<span><i><FaCheckDouble /></i></span>
+									</div>
+									<p>{item.name}</p>
 								</div>
-								<p>Urology</p>
-							</div>
-							<div className="speicality-item text-center">
-								<div className="speicality-img">
-									<img src={img2} className="img-fluid" alt="" />
-									<span><i><FaCheckDouble/></i></span>
-								</div>
-								<p>Neurology</p>
-							</div>
-							<div className="speicality-item text-center">
-								<div className="speicality-img">
-									<img src={img3} className="img-fluid" alt="" />
-									<span><i><FaCheckDouble/></i></span>
-								</div>
-								<p>Orthopedic</p>
-							</div>
-							<div className="speicality-item text-center">
-								<div className="speicality-img">
-									<img src={img4} className="img-fluid" alt="" />
-									<span><i><FaCheckDouble/></i></span>
-								</div>
-								<p>Cardiologist</p>
-							</div>
-							<div className="speicality-item text-center">
-								<div className="speicality-img">
-									<img src={img5} className="img-fluid" alt="" />
-									<span><i><FaCheckDouble/></i></span>
-								</div>
-								<p>Dentist</p>
-							</div>
+							))}
 						</div>
 					</div>
 				</div>
