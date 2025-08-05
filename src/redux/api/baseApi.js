e81d0cc78826e13989f2453bd44a7f0a2a56f3b5
John Sokpo <john.sokpo@hotmail.com>
2025-08-05 20:51:58 +0100

    Update baseApi.js

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -1,11 +1,17 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
-import { tagTypeList } from '../tag-types'
-import { axiosBaseQuery } from '../../helpers/axios/axiosBaseQuery'
-import { getBaseUrl } from '../../helpers/config/envConfig'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
-    reducerPath: 'api',
-    baseQuery: axiosBaseQuery({ baseUrl: getBaseUrl() }),
-    endpoints: () => ({}),
-    tagTypes: tagTypeList
-})
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://doctorpatientapi.onrender.com/api/v1',
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth?.token;
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['doctor'],
+  endpoints: () => ({}),
+});
