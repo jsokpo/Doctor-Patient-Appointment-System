4954c0ea7ccfb0bc5da96ca1f30df4adf1234696
John Sokpo <john.sokpo@hotmail.com>
2025-08-12 16:44:53 +0100

    Update baseApi.js

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://doctorpatientapi.onrender.com/api/v1',
+    baseUrl: 'https://healthservicebackend.onrender.com/api/v1',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth?.token;
       if (token) {
