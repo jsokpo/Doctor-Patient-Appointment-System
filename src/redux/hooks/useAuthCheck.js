2fa972fa92dc5856d87367ab52846024d5eb5812
John Sokpo <john.sokpo@hotmail.com>
2025-08-25 15:19:11 +0100

    Update useAuthCheck.js

--- src/redux/hooks/useAuthCheck.js
+++ src/redux/hooks/useAuthCheck.js
@@ -1,40 +1,41 @@
 import { useEffect, useState } from "react";
 import { useGetDoctorQuery } from "../api/doctorApi";
 import { useGetPatientQuery } from "../api/patientApi";
-import { getUserInfo } from '../../service/auth.service';
+import { getUserInfo } from "../../service/auth.service";
 
 export default function useAuthCheck() {
     const [authChecked, setAuthChecked] = useState(false);
     const [userId, setUserId] = useState('');
     const [isSkip, setIsSkip] = useState(true);
-    const [data, setData] = useState({});
     const [role, setRole] = useState("");
-    const { data: doctorData, isError, isSuccess: dIsSuccess } = useGetDoctorQuery(userId, { skip: isSkip });
+    
+    // Queries
+    const { data: doctorData, isError: dIsError, isSuccess: dIsSuccess } = useGetDoctorQuery(userId, { skip: isSkip });
     const { data: patientData, isError: pIsError, isSuccess: pIsSuccess } = useGetPatientQuery(userId, { skip: isSkip });
 
     useEffect(() => {
         const localAuth = getUserInfo();
 
         if (localAuth && localAuth !== null) {
-            if (localAuth.role === 'patient') {
-                setUserId(localAuth?.userId)
-                setIsSkip(false);
-                setData(patientData)
-                setRole(localAuth.role)
-                setAuthChecked(pIsSuccess && !pIsError)
-            } else if (localAuth.role === 'doctor') {
-                setUserId(localAuth?.userId)
-                setIsSkip(false);
-                setData(doctorData)
-                setRole(localAuth.role)
-                setAuthChecked(dIsSuccess && !isError)
-            }
+            const normalizedRole = localAuth.role?.toLowerCase();
+            setRole(normalizedRole);
+            setUserId(localAuth?.userId);
+            setIsSkip(false);
         }
-    }, [patientData, doctorData, isError, dIsSuccess, pIsError, pIsSuccess]);
+    }, []);
+
+    useEffect(() => {
+        if (role === "doctor" && dIsSuccess && !dIsError) {
+            setAuthChecked(true);
+        }
+        if (role === "patient" && pIsSuccess && !pIsError) {
+            setAuthChecked(true);
+        }
+    }, [role, dIsSuccess, dIsError, pIsSuccess, pIsError]);
 
     return {
         authChecked,
-        data,
+        data: role === "doctor" ? doctorData : patientData,
         role
     };
 }
