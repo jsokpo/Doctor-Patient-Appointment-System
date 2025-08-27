67947959893652bb5c6ca4a0385e18b69f8e9740
John Sokpo <john.sokpo@hotmail.com>
2025-08-27 19:20:52 +0100

    Update baseApi.js

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -1,17 +1,17 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'; 
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const baseApi = createApi({ 
-  reducerPath: 'api', 
-  baseQuery: fetchBaseQuery({ 
-    baseUrl: 'https://healthservicebackend.onrender.com/api/v1', 
-    prepareHeaders: (headers, { getState }) => { 
-      const token = getState().auth?.token; 
-      if (token) { 
-        headers.set('Authorization', Bearer ${token}); 
-      } 
-      return headers; 
-    }, 
-  }), 
-  tagTypes: ['doctor'], 
-  endpoints: () => ({}), 
+export const baseApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://healthservicebackend.onrender.com/api/v1',
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth?.token;
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`); // ✅ fixed
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['doctor'],
+  endpoints: () => ({}),
 });
